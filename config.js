9ca5d1437a8cdd5651229dc55a7eb8cd3c71a5f5
Nigel Mckenzie-Ryan <n.mckenryan@gmail.com>
2021-03-01 20:45:54 +1100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  siteTitle: 'Nigel Mckenzie-Ryan Resume', // <title>
+  manifestName: 'Resume',
+  manifestShortName: 'Landing', // max 12 characters
+  manifestStartUrl: '/',
+  manifestBackgroundColor: '#6a0001',
+  manifestThemeColor: '#6a0001',
+  manifestDisplay: 'standalone',
+  manifestIcon: 'src/assets/img/website-icon.png',
+  pathPrefix: `/nMckenryan.github.io/`, // This path is subpath of your hosting https://domain/portfolio
+  firstName: 'Nigel',
+  lastName: 'Mckenzie-Ryan',
+  // social
+  socialLinks: [
+    {
+      icon: 'fa-github',
+      name: 'Github',
+      url: 'https://github.com/nMckenryan',
+    },
+    {
+      icon: 'fa-linkedin-in',
+      name: 'Linkedin',
+      url: 'https://www.linkedin.com/in/nmckenryan/',
+    },
+  ],
+  email: 'n.mckenryan@gmail.com',
+  phone: 'Inquire for my Phone Number',
+  address: 'Auckland Central, New Zealand',
+};
